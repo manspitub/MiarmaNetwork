93d6605e697ef214a8eb574632ed0f65db733523
manspitub <74324580+manspitub@users.noreply.github.com>
2022-02-14 10:08:48 +0100

    Comienzo el proyecto

---
+++ MiarmaNetworkManuel/src/main/java/com/salesianos/dam/anuel/MiarmaNetwork/MiarmaNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.dam.anuel.MiarmaNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiarmaNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiarmaNetworkApplication.class, args);
+	}
+
+}
---
+++ MiarmaNetworkManuel/src/test/java/com/salesianos/dam/anuel/MiarmaNetwork/MiarmaNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.dam.anuel.MiarmaNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiarmaNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
