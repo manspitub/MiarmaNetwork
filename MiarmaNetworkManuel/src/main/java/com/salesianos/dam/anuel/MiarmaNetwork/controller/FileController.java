72118cbdb1d2715900e52dbc95d5eadd9025f2d1
manspitub <manspitub@hotmail.com>
2022-02-15 11:39:55 +0100

    Comienzo a implementar el user

--- MiarmaNetworkManuel/src/main/java/com/salesianos/dam/anuel/MiarmaNetwork/controller/FileController.java
+++ MiarmaNetworkManuel/src/main/java/com/salesianos/dam/anuel/MiarmaNetwork/controller/FileController.java
@@ -1,12 +1,21 @@
 package com.salesianos.dam.anuel.MiarmaNetwork.controller;
 
 
+import com.salesianos.dam.anuel.MiarmaNetwork.dto.FileResponse;
 import com.salesianos.dam.anuel.MiarmaNetwork.service.StorageService;
+import com.salesianos.dam.anuel.MiarmaNetwork.utils.MediaTypeUrlResource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +24,29 @@ public class FileController {
     private final StorageService storageService;
 
     public ResponseEntity<?> upload (@RequestPart("file")MultipartFile file){
-        return  ResponseEntity.ok().build();
+        String name = storageService.store(file);
+
+        String uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/download")
+                .path(name)
+                .toUriString();
+
+        FileResponse response = FileResponse.builder()
+                .name(name)
+                .size(file.getSize())
+                .type(file.getContentType())
+                .uri(uri)
+                .build();
+
+        return ResponseEntity.created(URI.create(uri)).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Resource> getFile (@PathVariable String filename) {
+        MediaTypeUrlResource resource = (MediaTypeUrlResource) storageService.loadAsResource(filename);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("content-type", resource.getType())
+                .body(resource);
     }
 }
---
+++ MiarmaNetworkManuel/src/main/java/com/salesianos/dam/anuel/MiarmaNetwork/users/model/User.java
@@ -0,0 +1,101 @@
+package com.salesianos.dam.anuel.MiarmaNetwork.users.model;
+
+import com.salesianos.dam.anuel.MiarmaNetwork.model.Publicacion;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.NaturalId;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import org.hibernate.annotations.Parameter;
+
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator",
+            parameters = {
+                    @Parameter(
+                            name = "uuid_gen_strategy_class",
+                            value = "org.hibernate.id.uuid.CustomVersionOneStrategy"
+                    )
+            }
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+
+    @NaturalId
+    @Column(unique = true, updatable = false)
+    private String nick;
+
+    private String avatar;
+
+    private boolean isPublic;
+
+    private LocalDateTime fechaNacimiento;
+
+    @Lob
+    private String biografia;
+
+    private String nombre;
+
+    public List<Publicacion> publicacionList;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
