9396cfb36a6a10bbbf6898f056c425477fd92b9a
manspitub <manspitub@hotmail.com>
2022-02-16 12:35:26 +0100

    Casi hecho los post de flutter

--- miarma_network/lib/screens/home_screen.dart
+++ miarma_network/lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -11,43 +11,150 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF9F9F9) ,
-      appBar: AppBar(
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(0.5), child: Container(
-            color: Colors.black38,
-            height: 0.75,
+        backgroundColor: Color(0xFFF9F9F9),
+        appBar: AppBar(
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(0.5),
+            child: Container(
+              color: Colors.black38,
+              height: 0.75,
+            ),
           ),
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Color(0xFFF9F9F9),
+          title: Container(
+            margin: EdgeInsets.only(top: 10),
+            child: SizedBox(
+                height: 60,
+                child: Image.network(
+                    "https://logos-marcas.com/wp-content/uploads/2020/04/Instagram-Logotipo-2016-Presente.png")),
+          ),
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.camera_alt,
+              color: Colors.black,
+              size: 30,
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.tv_sharp,
+                  color: Colors.black,
+                  size: 30,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.send_outlined,
+                  color: Colors.black,
+                  size: 30,
+                ))
+          ],
         ),
-        elevation: 0,
-        centerTitle: true,
-         backgroundColor: Color(0xFFF9F9F9),
-        title: Container(
-          margin: EdgeInsets.only(top: 10),
-          child: SizedBox(
-            height: 60,
-            child: Image.network("https://logos-marcas.com/wp-content/uploads/2020/04/Instagram-Logotipo-2016-Presente.png")),
-        ),
-        leading: IconButton(onPressed: (){}, icon: Icon(Icons.camera_alt, color: Colors.black, size: 30,),
-      ),
-      actions: <Widget>[
-        IconButton(onPressed: (){}, icon: Icon(Icons.tv_sharp, color: Colors.black, size: 30,)),
-        IconButton(onPressed: (){}, icon: Icon(Icons.send_outlined, color: Colors.black, size: 30,))
-      ],
-      
-    ),
-    body: Column(
-      children: <Widget>[
-        Container()
-      ],
-    )
-    );
-
-    
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 70,
+              ),
+              Divider(
+                color: Colors.grey,
+                height: 0,
+                thickness: 0.5,
+              ),
+              _post(),
+            ],
+          ),
+        ));
   }
 
-  
+  Widget _post() {
+    return Container(
+      height: 620,
+      child: ListView.builder(
+          itemCount: 50,
+          itemBuilder: (context, i) {
+            return _createPost();
+          }),
+      width: MediaQuery.of(context).size.width,
+    );
+  }
 
-  
+  Widget _createPost() {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  child: Container(
+                    padding: EdgeInsets.only(
+                        top: 12, left: 18, bottom: 12, right: 2),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image(
+                          image: NetworkImage(
+                              ('https://ceslava.s3-accelerate.amazonaws.com/2012/12/foto-perfil.jpg')),
+                          height: 65,
+                          width: 65,
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+                Text("Christian",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                Expanded(child: SizedBox()),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.more_horiz,
+                  ),
+                  iconSize: 30,
+                ),
+              ],
+            ),
+          ),
+          FadeInImage(
+            placeholder: AssetImage('assets/loading.gif'),
+            image: NetworkImage(
+              'https://dc722jrlp2zu8.cloudfront.net/media/teachers/luis-miguel-lopez-f2.webp',
+            ),
+            fit: BoxFit.cover,
+          ),
+          Container(
 
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.love)
+                  
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.more_horiz,
+                  ),
+                  iconSize: 30,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.more_horiz,
+                  ),
+                  iconSize: 30,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
